2f99a3af96adef5a8c0d423e22344edc9d293197
markanator <wowmarkambro@gmail.com>
2020-04-10 18:14:31 -0500

    started work, added wheel event and keydown event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+// WHEEL
+let bodyBgColor = document.getElementsByTagName("body")[0];
+console.log(bodyBgColor);
+bodyBgColor.addEventListener("wheel",(event)=>{
+    bodyBgColor.style.background = 'linear-gradient(#c5eafa, #fff)';
+});
+
+// KEYDOWN
+window.onscroll = function(ev) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        // you're at the bottom of the page
+        console.log("BOTTOM OF THE PAGE");
+    }
+};
