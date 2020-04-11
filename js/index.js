b2627258f064ebe8512afc4c3c2937be7c89fea9
markanator <wowmarkambro@gmail.com>
2020-04-10 20:04:40 -0500

    drag and drop added,load added, focus added..

--- js/index.js
+++ js/index.js
@@ -32,5 +32,67 @@ heroImg.addEventListener("mouseout",(event)=>{
 
 });
 
-let dragImg = document.getElementsByTagName("img")[1];
-console.log(dragImg);
+
+//DRAG and DROP
+//let dragImg = document.getElementsByTagName("img")[1];
+let allImages = Array.from(document.getElementsByTagName("img"));
+
+allImages.forEach((element)=>{
+    element.setAttribute("class","dropzone");
+
+    element.addEventListener("drag",(event)=>{
+    },false);
+    
+    element.addEventListener("dragend",(event)=>{
+        //reset
+        event.target.style.opacity = "";
+    },false);
+    
+    element.addEventListener("dragstart",(event)=>{
+        event.target.style.opacity = "0.3";
+    },false);
+    
+    element.addEventListener("dragover",(event)=>{
+        event.preventDefault();
+    },false);
+    
+    element.addEventListener("dragenter",(event)=>{
+        if(event.target.className == "dropzone"){
+            event.target.style.opacity = '0.5';
+        }
+    },false);
+    
+    element.addEventListener("dragleave", function(event) {
+        if (event.target.className == "dropzone") {
+            event.target.style.opacity = "";
+        }
+      }, false);
+    
+    
+      element.addEventListener("drop", function(event) {
+        event.preventDefault();
+        if (event.target.className == "dropzone") {
+            console.log("Dragged on an image!");
+            event.target.style.opacity = "";
+        }
+    }, false);
+
+
+});
+
+
+//LOAD
+window.addEventListener('load', (event) => {
+    document.body.style.color = "saddlebrown";
+});
+
+//FOCUS
+const anchors = Array.from(document.getElementsByTagName('a'));
+
+anchors.forEach((element)=>{
+    element.addEventListener('focus', (event) => {
+        event.target.style.background = 'orchid';    
+      });
+});
+
+//
